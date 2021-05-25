154fbb6eab514432ec31a194fbb7c6da2546a540
Mukovhe Lugisani <mlugisani@gmail.com>
2021-05-26 00:26:59 +0200

    Added API

--- regression-apples-predict-api-template/model.py
+++ regression-apples-predict-api-template/model.py
@@ -24,6 +24,7 @@
 # Helper Dependencies
 import numpy as np
 import pandas as pd
+from datetime import datetime
 import pickle
 import json
 
@@ -62,8 +63,8 @@ def _preprocess_data(data):
     
 
     feature_vector_df = feature_vector_df[(feature_vector_df['Commodities'] == 'APPLE GOLDEN DELICIOUS')]
-    predict_vector = feature_vector_df[['Total_Qty_Sold','Stock_On_Hand']]
-                                
+    predict_vector = feature_vector_df[['Weight_Kg', 'Low_Price', 'High_Price', 'Sales_Total', 'Total_Qty_Sold', 'Total_Kg_Sold', 'Stock_On_Hand']]
+
     # ------------------------------------------------------------------------
 
     return predict_vector
--- regression-apples-predict-api-template/utils/train_model.py
+++ regression-apples-predict-api-template/utils/train_model.py
@@ -12,7 +12,7 @@
 # Dependencies
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
 
 # Fetch training data and preprocess for modeling
 train = pd.read_csv('data/train_data.csv')
@@ -20,14 +20,14 @@ train = pd.read_csv('data/train_data.csv')
 train = train[(train['Commodities'] == 'APPLE GOLDEN DELICIOUS')]
 
 y_train = train['avg_price_per_kg']
-X_train = train[['Total_Qty_Sold','Stock_On_Hand']]
+X_train = train[['Weight_Kg', 'Low_Price', 'High_Price', 'Sales_Total', 'Total_Qty_Sold', 'Total_Kg_Sold', 'Stock_On_Hand']]
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+GBR = GradientBoostingRegressor(n_estimators = 280, min_samples_leaf = 4, max_depth = 7, learning_rate = 0.19183673469387755, random_state = 42)
 print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+GBR.fit(X_train, y_train)
 
 # Pickle model for use within our API
-save_path = '../assets/trained-models/apples_simple_lm_regression.pkl'
+save_path = '../assets/trained-models/apples_simple_GBR.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(GBR, open(save_path,'wb'))
